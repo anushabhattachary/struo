e10b191674bb9b1d94b50f7c7b3aa60e3b868622
Christie Peng <53117141+christiepeng@users.noreply.github.com>
2024-11-21 20:36:17 -0500

    Add files via upload

---
+++ views.py
@@ -0,0 +1,16 @@
+# accounts/views.py
+from .forms import CustomUserCreationForm
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+
+def signup(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')
+    else:
+        form = CustomUserCreationForm()
+    return render(request, 'registration/signup.html', {'form': form})
+
